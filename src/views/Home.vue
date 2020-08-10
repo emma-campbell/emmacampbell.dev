77c1c1689cefd6e1b1cf2de2f4a24b2e7e1cf4cd
ecampb10@u.rochester.edu <>
2020-08-10 14:57:35 -0400

    added urls

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="home">
-    <section id="intro" class="hero is-fullheight is-dark"  style="background-color: #0B132B">
+    <section id="intro" class="hero is-fullheight is-dark" style="background-color: #0B132B">
       <div class="hero-body" style="background-color: #1C2541">
         <div class="container has-text-centered" style="padding-bottom: 4em;">
           <img id="headshot" class="is-rounded" src="@/assets/headshot.jpg" />
           <h1 class="title">Hi 👋, my name is <b>Emma Campbell</b></h1>
           <p class="is-3"><i>Computer Science Student and Problem-Solver.</i></p>
           <div class="icons">
-            <b-icon id="home__icon" pack="fab" icon="github" size="is-large"></b-icon>
-            <b-icon id="home__icon" pack="fab" icon="linkedin" size="is-large"></b-icon>
-            <b-icon id="home__icon" pack="fas" icon="file" size="is-large"></b-icon>
+            <a href="https://github.com/emma-campbell">
+              <b-icon id="home__icon" pack="fab" icon="github" size="is-large"></b-icon>
+            </a>
+            <a href="https://www.linkedin.com/in/ecampbellur">
+              <b-icon id="home__icon" pack="fab" icon="linkedin" size="is-large"></b-icon>
+            </a>
+            <a href="https://drive.google.com/file/d/1pvA0tKpRvcvxEXkcw2KuSn7ZLRT7S_YY/view?usp=sharing">
+              <b-icon id="home__icon" pack="fas" icon="file" size="is-large"></b-icon>
+            </a>
           </div>
         </div>
       </div>
@@ -22,7 +28,8 @@
 .hero-body {
   padding: 0em;
   margin: 8em 12em;
-  border: 2px solid #6FFFE9;
+  max-width: 1000px;
+  border: 2px solid #6fffe9;
   z-index: 1;
 }
 
@@ -43,7 +50,7 @@
 
 #home__icon {
   margin-right: 20px;
-  color: #6FFFE9;
+  color: #6fffe9;
 }
 
 #particles-js {
