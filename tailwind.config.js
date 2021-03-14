6e5758ff65abeca0e6f41617d3cca9c51985ccea
emma-campbell <ecampb10@u.rochester.edu>
2021-03-14 16:12:21 -0400

    Added the colors I need

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
-      black: "#273036"
+      black: "#273036",
+      gray: colors.coolGray,
+      white: colors.white
     },
     fontFamily: {
       sans: ["Montserrat", "sans-serif"],
